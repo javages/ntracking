ec7531963f9236a36c471ac8a1893b1880181291
Neik <25412853+neilgcns@users.noreply.github.com>
2024-02-29 22:18:19 +0000

    Update sync_logs.sh
    upgrade to 50 machines

--- log_rotation/sync_logs.sh
+++ log_rotation/sync_logs.sh
@@ -43,6 +43,26 @@ mkdir -p "$DEST_BASE_DIR/s27"
 mkdir -p "$DEST_BASE_DIR/s28"
 mkdir -p "$DEST_BASE_DIR/s29"
 mkdir -p "$DEST_BASE_DIR/s30"
+mkdir -p "$DEST_BASE_DIR/s31"
+mkdir -p "$DEST_BASE_DIR/s32"
+mkdir -p "$DEST_BASE_DIR/s33"
+mkdir -p "$DEST_BASE_DIR/s34"
+mkdir -p "$DEST_BASE_DIR/s35"
+mkdir -p "$DEST_BASE_DIR/s36"
+mkdir -p "$DEST_BASE_DIR/s37"
+mkdir -p "$DEST_BASE_DIR/s38"
+mkdir -p "$DEST_BASE_DIR/s39"
+mkdir -p "$DEST_BASE_DIR/s40"
+mkdir -p "$DEST_BASE_DIR/s41"
+mkdir -p "$DEST_BASE_DIR/s42"
+mkdir -p "$DEST_BASE_DIR/s43"
+mkdir -p "$DEST_BASE_DIR/s44"
+mkdir -p "$DEST_BASE_DIR/s45"
+mkdir -p "$DEST_BASE_DIR/s46"
+mkdir -p "$DEST_BASE_DIR/s47"
+mkdir -p "$DEST_BASE_DIR/s48"
+mkdir -p "$DEST_BASE_DIR/s49"
+mkdir -p "$DEST_BASE_DIR/s50"
 
 # for local machin server
 mkdir -p "$DEST_BASE_DIR/s00"
@@ -80,11 +100,31 @@ rsync -avz --update s22:$HOME/.local/share/local_machine/resources_*.log $DEST_B
 rsync -avz --update s23:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s23/
 rsync -avz --update s24:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s24/
 rsync -avz --update s25:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s25/
-rsync -avz --update s28:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s26/
+rsync -avz --update s26:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s26/
 rsync -avz --update s27:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s27/
 rsync -avz --update s28:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s28/
 rsync -avz --update s29:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s29/
 rsync -avz --update s30:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s30/
+rsync -avz --update s31:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s31/
+rsync -avz --update s32:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s32/
+rsync -avz --update s33:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s33/
+rsync -avz --update s34:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s34/
+rsync -avz --update s35:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s35/
+rsync -avz --update s36:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s36/
+rsync -avz --update s37:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s37/
+rsync -avz --update s38:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s38/
+rsync -avz --update s39:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s39/
+rsync -avz --update s40:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s40/
+rsync -avz --update s41:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s41/
+rsync -avz --update s42:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s42/
+rsync -avz --update s43:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s43/
+rsync -avz --update s44:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s44/
+rsync -avz --update s45:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s45/
+rsync -avz --update s46:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s46/
+rsync -avz --update s47:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s47/
+rsync -avz --update s48:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s48/
+rsync -avz --update s49:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s49/
+rsync -avz --update s50:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s50/
 
 echo "Synchronization script completed."
 
